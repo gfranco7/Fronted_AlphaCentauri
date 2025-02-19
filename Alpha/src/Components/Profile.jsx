888fc3aa1f56e68f5f9fb7323d8b8ab1b7cbea8d
gfranco7 <deepamgf710@gmail.com>
2025-02-18 23:59:42 -0500

    Art

--- Alpha/src/Components/Profile.jsx
+++ Alpha/src/Components/Profile.jsx
@@ -15,7 +15,8 @@ export const Profile = () => {
         <AsideMain usuario={usuario} />
         <div className=' w-full h-full overflow-y-auto'
             style={{ 
-              backgroundImage: `url('https://www.oecd.org/content/dam/oecd/en/topics/policy-issues/space-economy/shutterstock_1389413753.jpg ')`,
+              backgroundImage: `url('
+              ')`,
               backgroundSize: 'cover', // Ajusta la imagen para cubrir todo el fondo
               backgroundPosition: 'center', // Centra la imagen en el fondo
               backgroundRepeat: 'no-repeat' // Evita que la imagen se repita
